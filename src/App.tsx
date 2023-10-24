b62a1d484ed3ac86c67c2261b59b9393ebb54c4d
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-10-24 20:53:49 -0300

    Projeto iniciado

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/styles/custom.scss';
+import './App.css';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    </>
+  );
+};
+
+export default App;
